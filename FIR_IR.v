26271b981b202cb96dfcd5baf557a4b8145dbae6
Ooo1nxW <118484951+Ooo1nxW@users.noreply.github.com>
2024-08-16 13:19:31 +0200

    delect loop

--- FIR_IR.v
+++ FIR_IR.v
@@ -36,85 +36,107 @@ reg  [7:0] in_shift [21:0];
 reg  [19:0] mul_reg [10:0]; 
 // reg  [19:0] add_reg [10:0];
 
-reg [7:0] i,j;
-reg [7:0] k;
-reg [2:0] en;
+// reg [7:0] i,j;
+// reg [7:0] k;
+// reg [2:0] en;
 
 reg [19:0] add_temp1;
 reg [19:0] add_temp2;
 
-// en[2:0] shift
-always @(posedge CLK_Filter or negedge rst_n) begin
-	if(!rst_n)begin
-		en[2:0] <= 3'b001;
-		k <= 0;
-	end else begin
-		k <= k+1;
-		if (k<=1) begin
-			en[2:0] <= {en[1:0], 1'b0};
-		end
-		else begin
-			k <= 0;
-			en[2:0] <= 3'b001;
-		end
-	end
-end
 
 //22 shift_register, in_shift [21:0]
 always @(posedge CLK_Filter or negedge rst_n) begin
 	if(!rst_n)begin
-		for (i=0; i<=21; i=i+1) begin
-			in_shift[i] <= 7'd0; 
-		end
-		//en[2:0] <= 3'b001;
+		in_shift[0] <= 7'd0;
+		in_shift[1] <= 7'd0;
+		in_shift[2] <= 7'd0;
+		in_shift[3] <= 7'd0;
+		in_shift[4] <= 7'd0;
+		in_shift[5] <= 7'd0;
+		in_shift[6] <= 7'd0;
+		in_shift[7] <= 7'd0;
+		in_shift[8] <= 7'd0;
+		in_shift[9] <= 7'd0;
+		in_shift[10] <= 7'd0;
+		in_shift[11] <= 7'd0;
+		in_shift[12] <= 7'd0;
+		in_shift[13] <= 7'd0;
+		in_shift[14] <= 7'd0;
+		in_shift[15] <= 7'd0;
+		in_shift[16] <= 7'd0;
+		in_shift[17] <= 7'd0;
+		in_shift[18] <= 7'd0;
+		in_shift[19] <= 7'd0;
+		in_shift[20] <= 7'd0;
+		in_shift[21] <= 7'd0; 
 	end 
-	else if (en[0]) begin
+	else begin
 		in_shift[0] <= IR_ADC_Value;
-		for (i=0; i<=21; i=i+1) begin
-			in_shift [i+1] <= in_shift[i];
-			//$timeformat(-3, 0, "ns"); 
-			//$display("in_shift %b",in_shift[i]);
-		end
-		// en[2:0] <= {en[1:0], 1'b0};
+		in_shift[1] <= in_shift[0];
+		in_shift[2] <= in_shift[1];
+		in_shift[3] <= in_shift[2];
+		in_shift[4] <= in_shift[3];
+		in_shift[5] <= in_shift[4];
+		in_shift[6] <= in_shift[5];
+		in_shift[7] <= in_shift[6];
+		in_shift[8] <= in_shift[7];
+		in_shift[9] <= in_shift[8];
+		in_shift[10] <= in_shift[9];
+		in_shift[11] <= in_shift[10];
+		in_shift[12] <= in_shift[11];
+		in_shift[13] <= in_shift[12];
+		in_shift[14] <= in_shift[13];
+		in_shift[15] <= in_shift[14];
+		in_shift[16] <= in_shift[15];
+		in_shift[17] <= in_shift[16];
+		in_shift[18] <= in_shift[17];
+		in_shift[19] <= in_shift[18];
+		in_shift[20] <= in_shift[19];
+		in_shift[21] <= in_shift[20];
+		
 	end
 end
 				
 //ADDER	
 always @(posedge CLK_Filter or negedge rst_n) begin
-	if(!rst_n)begin
-		for (j=0; j<=10; j=j+1) begin
-			mul_reg[j] <= 20'd0;
-			//add_reg[j] <= 20'd0;
-		end
+	if(!rst_n)begin		
+			mul_regj[0] <= 20'd0;
+			mul_regj[1] <= 20'd0;
+			mul_regj[2] <= 20'd0;
+			mul_regj[3] <= 20'd0;
+			mul_regj[4] <= 20'd0;
+			mul_regj[5] <= 20'd0;
+			mul_regj[6] <= 20'd0;
+			mul_regj[7] <= 20'd0;
+			mul_regj[8] <= 20'd0;
+			mul_regj[9] <= 20'd0;
+			mul_regj[10] <= 20'd0;
 	end 
-	else if (en[1])begin
-		for (j=0; j<=10; j=j+1) begin
-			mul_reg[j] <= coeff[j] * (in_shift [j] + in_shift [21-j]); 
-			
-			
-			// $display("mul_reg %b",mul_reg[j]);
-			// $display("coeff %b",coeff[j]);
-		end
-		//en[2:0] <= {en[1:0], 1'b0};
+	else begin		
+			mul_reg[0] <= coeff[0] * (in_shift [0] + in_shift [21]); 
+			mul_reg[1] <= coeff[1] * (in_shift [1] + in_shift [20]); 
+			mul_reg[2] <= coeff[2] * (in_shift [2] + in_shift [19]); 
+			mul_reg[3] <= coeff[3] * (in_shift [3] + in_shift [18]); 
+			mul_reg[4] <= coeff[4] * (in_shift [4] + in_shift [17]); 
+			mul_reg[5] <= coeff[5] * (in_shift [5] + in_shift [16]); 
+			mul_reg[6] <= coeff[6] * (in_shift [6] + in_shift [15]); 
+			mul_reg[7] <= coeff[7] * (in_shift [7] + in_shift [14]); 
+			mul_reg[8] <= coeff[8] * (in_shift [8] + in_shift [13]); 
+			mul_reg[9] <= coeff[9] * (in_shift [9] + in_shift [12]); 
+			mul_reg[10] <= coeff[10] * (in_shift [10] + in_shift[11]); 			
 	end
 end
 
-
 always @(posedge CLK_Filter or negedge rst_n) begin
-
 	if(!rst_n)begin
 		Out_IR_Filtered <= 20'd0;
+		add_temp1 <= 19'd0;
+		add_temp2 <= 19'd0;
 	end 
-	else if (en[2]) begin
-		//$display ("enter!IR");
-		//for (j=0; j<=10; j=j+1) begin
-			//$display ("enter!RED forloop");
+	else begin
 		add_temp1 <=  mul_reg[0] + mul_reg[1] + mul_reg[2] + mul_reg[3] + mul_reg[4] + mul_reg[5];
-			//$display("Out_IR_Filtered %b",Out_IR_Filtered);
 		add_temp2 <=  mul_reg[6] + mul_reg[7] + mul_reg[8] + mul_reg[9] + mul_reg[10];
 		Out_IR_Filtered <= add_temp1 + add_temp2;
-		//end
-		//en[2:0] <= {en[1:0], 1'b1};
 	end
 end
 
