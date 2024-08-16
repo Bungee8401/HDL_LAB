f7fa689bf1faf4721570a833903df5309b2c0f00
Ooo1nxW <118484951+Ooo1nxW@users.noreply.github.com>
2024-08-16 15:13:47 +0200

    FIR_IR

---
+++ Controller_mit_comman.v
@@ -0,0 +1,426 @@
+`timescale 1ms/1ps
+
+module Controller_mit_command (
+    input  [7:0] ADC,
+    input  Find_setting,
+    input  CLK,
+    input  rst_n,
+
+    output reg [3:0] LED_DRIVE,
+    output reg [6:0] DC_Comp,
+    output reg LED_IR,
+    output reg LED_RED,
+    output reg [3:0] PGA_Gain,
+    output reg CLK_Filter,
+    output reg [7:0] IR_ADC_Value,
+    output reg [7:0] RED_ADC_Value
+);
+    
+    reg [7:0] current_state;
+    reg [7:0] next_state;
+
+    reg [6:0] RED_DC_Comp;
+    reg [6:0] IR_DC_Comp;
+
+    reg [3:0] RED_PGA;
+    reg [3:0] IR_PGA;
+
+
+    reg [7:0] V_max;
+    reg [7:0] V_min;
+    reg [8:0] average;
+
+    reg [10:0] i;
+    reg [3:0] timer;
+    reg [12:0] adc_sum;
+
+    parameter INITIAL   = 8'b1000_0000; //0
+    parameter DC_RED    = 8'b0000_0001;//1
+    parameter PGA_RED   = 8'b0000_0010;//2
+    parameter DC_IR     = 8'b0000_0100;//4
+    parameter PGA_IR    = 8'b0000_1000;//8
+    parameter OPERATION = 8'b0001_0000;//16
+
+    // parameter PGA_RED_IN = 8'b0001_0001; //17
+    // parameter PGA_RED_OUT = 8'b0001_0010;//18
+    // parameter PGA_IR_IN = 8'b0001_0011;//19
+    // parameter PGA_IR_OUT = 8'b0001_0100;//20
+
+    parameter ONE_ADC_PERIOD = 1000;
+    parameter HALF_ADC_PERIOD = 500; //PAG Counter
+    parameter TWENTY_ADC_SAMPLE = 20; //DC Counter
+
+    //clk_filter
+    always @(posedge CLK or negedge rst_n) begin
+        if(~rst_n)
+            CLK_Filter <= 1'd0;
+        else
+            CLK_Filter <= ~CLK_Filter;
+    end    
+
+
+    // FSM state transition at positive CLK 
+    always @(posedge CLK or negedge rst_n) begin
+        if (~rst_n) begin
+            current_state <= INITIAL;
+        end
+        else if (Find_setting) begin 
+            current_state <= DC_RED;
+        end
+        else begin
+            current_state <= next_state;
+        end
+    end
+
+    // state output 
+    always @(posedge CLK) begin 
+        case(current_state) 
+            
+            INITIAL: begin              
+                //@(negedge CLK)
+                //CLK_Filter = 1'b0;
+                LED_DRIVE = 4'd10;  // fixed now, TODO later
+                DC_Comp = 7'd127;	// start somewhere
+                LED_IR = 1'b0;
+                LED_RED = 1'b0;
+                PGA_Gain = 4'd0;
+                adc_sum = 13'b0;    
+
+                RED_DC_Comp = 7'b0;
+                IR_DC_Comp = 7'b0;
+                RED_PGA = 4'b0;
+                IR_PGA = 4'b0;
+
+                // Find_setting_Complete = 1'b0;
+
+                V_min = 255;
+                V_max = 0;
+                average = 0;
+                i = 0;
+                timer = 0;
+
+                //Out_IR_Filtered = 20'b0;
+                //Out_RED_Filtered = 20'b0;
+
+                next_state = DC_RED; //next_state = DC_RED;
+            end
+
+            DC_RED: begin // DC comb for RED 
+                
+                LED_RED = 1'b1;
+                LED_IR = 1'b0;
+
+                if (i<HALF_ADC_PERIOD) begin
+                    //@ (negedge CLK); 
+                    V_max = (ADC > V_max)?  ADC:V_max;
+                    V_min = (ADC < V_min)?  ADC:V_min;
+                    
+                    i=i+1;
+                end
+                else begin
+                    i=0;
+                    average = (V_max + V_min) >>1; 
+                    if (average<116) begin
+                        DC_Comp = DC_Comp - 7'd4;
+                        // DC_Comp <= DC_Comp - ((DC_Comp-7'd90)>>1);
+                        // DC_Comp = DC_Comp - ((120-average)>>1);
+
+                        V_max = 0;
+                        V_min = 255;
+                    end
+                        
+                    else if (average>140) begin
+                        DC_Comp = DC_Comp + 7'd3;
+                        // DC_Comp <= DC_Comp + ((DC_Comp-7'd90)>>1);
+                        // DC_Comp = DC_Comp + ((135-average)>>1);
+
+                        V_max = 0;
+                        V_min = 255;
+                    end
+                        
+                    else begin
+                        next_state = PGA_RED;
+                        RED_DC_Comp = DC_Comp;
+                        V_max = 0;
+                        V_min = 255;
+                        average = 9'd0;
+                        // PGA_Gain <= 4'd0;
+                    end
+                end
+            end 
+
+            PGA_RED: begin
+                    if (i<HALF_ADC_PERIOD) begin
+                        V_min = (ADC < V_min) ? ADC : V_min;
+                        V_max = (ADC > V_max) ? ADC : V_max;
+                        i=i+1;
+                    end
+                    else begin
+                        i=0;
+                        if (10<V_min && V_max<245) begin
+                            V_max = 0;
+                            V_min = 255; 
+                            PGA_Gain = PGA_Gain + 4'd1;
+                            next_state = PGA_RED;                            
+                        end
+                        
+                        if (V_min<10 || V_max>245 ) begin  
+                            V_max = 0;
+                            V_min = 255; 
+                            // PGA_Gain <= PGA_Gain - 4'b1;
+                            RED_PGA = PGA_Gain - 4'd1;
+                            // next_state <= PGA_RED_OUT;
+                            next_state = DC_IR;
+                            PGA_Gain = 4'd0;
+                            DC_Comp =  7'd30;                     
+                        end
+                    end   
+            end
+
+            // PGA_RED_IN: begin
+            //             if (i<HALF_ADC_PERIOD) begin
+            //             V_min <= (ADC < V_min) ? ADC : V_min;
+            //             V_max <= (ADC > V_max) ? ADC : V_max;
+            //             i<=i+1;
+            //         end
+            //         else begin
+            //             i=0;
+            //             if (10<V_min && V_max<245) begin
+            //                 PGA_Gain <= PGA_Gain + 4'b1;
+            //                 V_max <= 0;
+            //                 V_min <= 255;
+            //             end
+                        
+            //             if (V_min<10 || V_max>245 ) begin  
+                            
+            //                 next_state <= DC_IR;                            
+            //                 RED_PGA <= PGA_Gain - 4'b1;
+            //                 PGA_Gain <= 0;
+            //                 V_max <= 0;
+            //                 V_min <= 255;
+            //                 average <= 0; //?????IRDC????counter
+            //                 DC_Comp <= 100;  //?IR?????????????
+            //             end
+            //     end
+            // end
+                
+            // PGA_RED_OUT: begin
+            //             if (i<HALF_ADC_PERIOD) begin
+            //             V_min <= (ADC < V_min) ? ADC : V_min;
+            //             V_max <= (ADC > V_max) ? ADC : V_max;
+            //             i<=i+1;
+            //         end
+            //         else begin
+            //             i=0;
+            //             if (10<V_min && V_max<245) begin
+            //                 next_state <= DC_IR;
+            //                 //@ (negedge CLK);
+            //                 RED_PGA <= PGA_Gain;
+            //                 PGA_Gain <= 0;
+                            
+            //                 V_max <= 0;
+            //                 V_min <= 255;  
+            //                 DC_Comp <= 100;  //?IN????
+            //                 average <= 0;                     
+            //             end
+                        
+            //             if (V_min<10 || V_max>245 ) begin  
+            //                 PGA_Gain <= PGA_Gain - 4'b1;
+            //                 V_max <= 0;
+            //                 V_min <= 255; 
+            //             end
+            //     end
+            // end                          
+
+            DC_IR: begin // DC comb for IR 
+                LED_RED = 1'b0;
+                LED_IR = 1'b1;
+
+                if (i<HALF_ADC_PERIOD) begin
+                    //@ (negedge CLK); 
+                    V_max = (ADC > V_max)?  ADC:V_max;
+                    V_min = (ADC < V_min)?  ADC:V_min;                   
+                    i=i+1;
+                end
+                else begin
+                    i=0;
+                    average = (V_max + V_min) >>1; 
+                    if (average<116) begin
+                        DC_Comp = DC_Comp - 7'd4; 
+                        // DC_Comp <= DC_Comp - ((DC_Comp-7'd90)>>1);
+                        // DC_Comp = DC_Comp - ((120-average)>>1); 
+
+                        V_max = 0;
+                        V_min = 255;
+
+                    end
+                        
+                    else if (average>140) begin
+                        DC_Comp = DC_Comp + 7'd3; 
+                        // DC_Comp <= DC_Comp + ((DC_Comp-7'd90)>>1); 
+                        // DC_Comp = DC_Comp + ((135-average)>>1);  
+
+                        V_max = 0;
+                        V_min = 255;
+
+                    end
+                
+                    else begin  
+                        next_state = PGA_IR;
+                        // @ (negedge CLK);
+                        IR_DC_Comp = DC_Comp;
+                        V_max = 0;
+                        V_min = 255;
+                        // DC_Comp = 0;
+                        PGA_Gain = 4'd0;
+                        average = 9'd0;
+                    end
+                end
+            end 
+
+            PGA_IR: begin
+                    if (i<HALF_ADC_PERIOD) begin
+                        V_min = (ADC < V_min) ? ADC : V_min;
+                        V_max = (ADC > V_max) ? ADC : V_max;
+                        i = i+1;
+                    end
+                    else begin
+                        i=0;
+                        if (10<V_min && V_max<245) begin
+                            V_max = 0;
+                            V_min = 255; 
+                            PGA_Gain = PGA_Gain + 4'b1;
+                            next_state = PGA_IR;                            
+                        end
+                        
+                        if (V_min<10 || V_max>245 ) begin  
+                            V_max = 0;
+                            V_min = 255; 
+                            IR_PGA = PGA_Gain - 4'b1;
+                            next_state = OPERATION;
+                            PGA_Gain = 4'b0;
+                                                
+                        end
+                    end   
+            end
+
+            // PGA_IR_IN: begin
+            //             if (i<HALF_ADC_PERIOD) begin
+            //             V_min <= (ADC < V_min) ? ADC : V_min;
+            //             V_max <= (ADC > V_max) ? ADC : V_max;
+            //             i <= i+1;
+            //         end
+            //         else begin
+            //             i=0;
+            //             if (10<V_min && V_max<245) begin
+            //                 PGA_Gain <= PGA_Gain + 4'b1;
+            //                 V_max <= 0;
+            //                 V_min <= 255;
+            //             end
+                        
+            //             if (V_min<10 || V_max>245 ) begin  
+                            
+            //                 next_state <= OPERATION;
+            //                 // @ (negedge CLK);
+            //                 IR_PGA <= PGA_Gain - 4'b1;
+            //                 PGA_Gain <= 0;
+            //                 V_max <= 0;
+            //                 V_min <= 255;
+
+            //                 DC_Comp <= 100;  //?IN????
+            //                 average <= 0;
+            //             end
+            //     end
+            // end
+
+                
+            // PGA_IR_OUT: begin
+            //             if (i<HALF_ADC_PERIOD) begin
+            //             V_min <= (ADC < V_min) ? ADC : V_min;
+            //             V_max <= (ADC > V_max) ? ADC : V_max;
+            //             i<=i+1;
+            //         end
+            //         else begin
+            //             i<=0;
+            //             if (10<V_min && V_max<245) begin
+            //                 next_state <= OPERATION;
+            //                 // @ (negedge CLK);
+            //                 IR_PGA <= PGA_Gain;
+            //                 PGA_Gain <= 0;
+            //                 DC_Comp <= 100;  //?100??????????????????????????????????????
+            //                 average <= 0;
+            //                 V_max <= 0;
+            //                 V_min <= 255;                       
+            //             end
+                        
+            //             if (V_min<10 || V_max>245 ) begin  
+            //                 PGA_Gain <= PGA_Gain - 4'b1;
+            //                 V_max <= 0;
+            //                 V_min <= 255; 
+            //             end
+            //     end
+            // end
+
+            OPERATION: begin
+		        //Find_setting_Complete  = 1'b1;      // flag signal for LED switching block
+                //if(Find_setting_Complete) begin // setting found, switch faster -> 100Hz, 10ms       
+                    if(timer == 9) begin
+                        timer = 0;
+                        LED_RED = ~LED_RED;
+                        LED_IR = ~LED_IR;	
+	 		            if (LED_RED == 1 && LED_IR == 0) begin
+                            PGA_Gain = RED_PGA;
+                            DC_Comp = RED_DC_Comp;
+                        end 
+                        if (LED_RED == 0 && LED_IR == 1) begin
+                            PGA_Gain = IR_PGA;
+                            DC_Comp = IR_DC_Comp; 			
+                    	end 
+		    end
+		    else begin
+                        timer = timer + 1;       
+                        if (LED_RED == 1 && LED_IR == 0) begin
+                            RED_ADC_Value = ADC;
+        
+                        end 
+
+                        if (LED_RED == 0 && LED_IR == 1) begin
+                            IR_ADC_Value = ADC;
+                    
+                        end
+                    end
+                end
+		    
+
+            default:    next_state = INITIAL ;
+        
+        endcase
+    end
+
+
+    // always @(posedge CLK) begin  
+    //     if(Find_setting_Complete) begin // setting found, switch faster -> 100Hz, 10ms       
+    //         if(timer == 9) begin
+    //             timer <= 0;
+    //             LED_RED <= ~LED_RED;
+    //             LED_IR <= ~LED_IR;
+    //         end else begin
+    //             timer <= timer + 1;       
+    //             if (LED_RED == 1 && LED_IR == 0) begin
+    //                 RED_ADC_Value <= ADC;
+    //                 PGA_Gain <= RED_PGA;
+    //                 DC_Comp <= RED_DC_Comp;
+    //             end 
+
+    //             if (LED_RED == 0 && LED_IR == 1) begin
+    //                 IR_ADC_Value <= ADC;
+    //                 PGA_Gain <= IR_PGA;
+    //                 DC_Comp <= IR_DC_Comp;           
+    //             end
+    //         end
+    //     end
+        
+    // end
+endmodule
+
+ 
--- FIR_IR.v
+++ FIR_IR.v
@@ -13,7 +13,6 @@ module FIR_IR (
 	input CLK_Filter,
 	input rst_n,
 	input  [7:0] IR_ADC_Value,
-	
 	output reg [19:0] Out_IR_Filtered
 	);
 
@@ -32,44 +31,57 @@ assign coeff[8]=8'd111;
 assign coeff[9]=8'd122;
 assign coeff[10]=8'd128;
 
-reg  [7:0] in_shift [21:0]; 
-reg  [19:0] mul_reg [10:0]; 
-// reg  [19:0] add_reg [10:0];
+// define input shift register
+reg [7:0] in_shift [21:0]; 
+reg [19:0] shift_buf [10:0];
 
-// reg [7:0] i,j;
-// reg [7:0] k;
-// reg [2:0] en;
+//define multipler
+reg [19:0] mul_reg [10:0]; 
 
-reg [19:0] add_temp1;
+//define sum register
+reg [19:0] add_temp1; 
 reg [19:0] add_temp2;
 
 
 //22 shift_register, in_shift [21:0]
 always @(posedge CLK_Filter or negedge rst_n) begin
 	if(!rst_n)begin
-		in_shift[0] <= 7'd0;
-		in_shift[1] <= 7'd0;
-		in_shift[2] <= 7'd0;
-		in_shift[3] <= 7'd0;
-		in_shift[4] <= 7'd0;
-		in_shift[5] <= 7'd0;
-		in_shift[6] <= 7'd0;
-		in_shift[7] <= 7'd0;
-		in_shift[8] <= 7'd0;
-		in_shift[9] <= 7'd0;
-		in_shift[10] <= 7'd0;
-		in_shift[11] <= 7'd0;
-		in_shift[12] <= 7'd0;
-		in_shift[13] <= 7'd0;
-		in_shift[14] <= 7'd0;
-		in_shift[15] <= 7'd0;
-		in_shift[16] <= 7'd0;
-		in_shift[17] <= 7'd0;
-		in_shift[18] <= 7'd0;
-		in_shift[19] <= 7'd0;
-		in_shift[20] <= 7'd0;
-		in_shift[21] <= 7'd0; 
+		in_shift[0] <= 8'd0;
+		in_shift[1] <= 8'd0;
+		in_shift[2] <= 8'd0;
+		in_shift[3] <= 8'd0;
+		in_shift[4] <= 8'd0;
+		in_shift[5] <= 8'd0;
+		in_shift[6] <= 8'd0;
+		in_shift[7] <= 8'd0;
+		in_shift[8] <= 8'd0;
+		in_shift[9] <= 8'd0;
+		in_shift[10] <= 8'd0;
+		in_shift[11] <= 8'd0;
+		in_shift[12] <= 8'd0;
+		in_shift[13] <= 8'd0;
+		in_shift[14] <= 8'd0;
+		in_shift[15] <= 8'd0;
+		in_shift[16] <= 8'd0;
+		in_shift[17] <= 8'd0;
+		in_shift[18] <= 8'd0;
+		in_shift[19] <= 8'd0;
+		in_shift[20] <= 8'd0;
+		in_shift[21] <= 8'd0; 
+		shift_buf [0] <= 20'd0;
+		shift_buf [1] <= 20'd0;
+		shift_buf [2] <= 20'd0;
+		shift_buf [3] <= 20'd0;
+		shift_buf [4] <= 20'd0;
+		shift_buf [5] <= 20'd0;
+		shift_buf [6] <= 20'd0;
+		shift_buf [7] <= 20'd0;
+		shift_buf [8] <= 20'd0;
+		shift_buf [9] <= 20'd0;
+		shift_buf [10] <= 20'd0;
+
 	end 
+
 	else begin
 		in_shift[0] <= IR_ADC_Value;
 		in_shift[1] <= in_shift[0];
@@ -93,6 +105,18 @@ always @(posedge CLK_Filter or negedge rst_n) begin
 		in_shift[19] <= in_shift[18];
 		in_shift[20] <= in_shift[19];
 		in_shift[21] <= in_shift[20];
+
+		shift_buf [0] <= in_shift [0] + in_shift [21];
+		shift_buf [1] <= in_shift [1] + in_shift [20];
+		shift_buf [2] <= in_shift [2] + in_shift [19];
+		shift_buf [3] <= in_shift [3] + in_shift [18];
+		shift_buf [4] <= in_shift [4] + in_shift [17];
+		shift_buf [5] <= in_shift [5] + in_shift [16];
+		shift_buf [6] <= in_shift [6] + in_shift [15];
+		shift_buf [7] <= in_shift [7] + in_shift [14];
+		shift_buf [8] <= in_shift [8] + in_shift [13];
+		shift_buf [9] <= in_shift [9] + in_shift [12];
+		shift_buf [10] <= in_shift [10] + in_shift[11];
 		
 	end
 end
@@ -111,19 +135,20 @@ always @(posedge CLK_Filter or negedge rst_n) begin
 			mul_reg[8] <= 20'd0;
 			mul_reg[9] <= 20'd0;
 			mul_reg[10] <= 20'd0;
+			
 	end 
 	else begin		
-			mul_reg[0] <= coeff[0] * (in_shift [0] + in_shift [21]); 
-			mul_reg[1] <= coeff[1] * (in_shift [1] + in_shift [20]); 
-			mul_reg[2] <= coeff[2] * (in_shift [2] + in_shift [19]); 
-			mul_reg[3] <= coeff[3] * (in_shift [3] + in_shift [18]); 
-			mul_reg[4] <= coeff[4] * (in_shift [4] + in_shift [17]); 
-			mul_reg[5] <= coeff[5] * (in_shift [5] + in_shift [16]); 
-			mul_reg[6] <= coeff[6] * (in_shift [6] + in_shift [15]); 
-			mul_reg[7] <= coeff[7] * (in_shift [7] + in_shift [14]); 
-			mul_reg[8] <= coeff[8] * (in_shift [8] + in_shift [13]); 
-			mul_reg[9] <= coeff[9] * (in_shift [9] + in_shift [12]); 
-			mul_reg[10] <= coeff[10] * (in_shift [10] + in_shift[11]); 			
+			mul_reg[0] <= coeff[0] * shift_buf [0]; 
+			mul_reg[1] <= coeff[1] * shift_buf [1];
+			mul_reg[2] <= coeff[2] * shift_buf [2];
+			mul_reg[3] <= coeff[3] * shift_buf [3]; 
+			mul_reg[4] <= coeff[4] * shift_buf [4]; 
+			mul_reg[5] <= coeff[5] * shift_buf [5]; 
+			mul_reg[6] <= coeff[6] * shift_buf [6]; 
+			mul_reg[7] <= coeff[7] * shift_buf [7]; 
+			mul_reg[8] <= coeff[8] * shift_buf [8]; 
+			mul_reg[9] <= coeff[9] * shift_buf [9]; 
+			mul_reg[10] <= coeff[10] * shift_buf [10]; 			
 	end
 end
 
